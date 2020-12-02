cf9d07b1756c1cdeed438f96101c172be949bd56
gyurk <gyurk@DESKTOP-Q7MUI9K>
2020-12-02 16:14:23 +0100

    Létrhoztam két tesztelést. Az egyik 16 való osztás, a másik 5 való osztás. Kiss György

---
+++ test/Osztás_tesztelés.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import faktoriálisprojekt.Osztás_kivetel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class Osztás_tesztelés {
+    
+    public Osztás_tesztelés() {
+    }
+    
+    @Test
+    public void osztás16() throws Osztás_kivetel {
+        int oszto = 64;
+        int nevezo = 16;
+        int elvárás = 4;
+        int eredmény = faktoriálisprojekt.Osztás.osztás(oszto, nevezo);
+        assertEquals(eredmény, elvárás);
+    }
+    
+    @Test
+    public void osztás5() throws Osztás_kivetel {
+        int oszto = 54;
+        int nevezo = 5;
+        int elvárás = 11;
+        int eredmény = faktoriálisprojekt.Osztás.osztás(oszto, nevezo);
+        assertEquals(eredmény, elvárás);
+    }
+}
